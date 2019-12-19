84a44a90bc652f268e927435a76593d0c5ab8885
Aluno18 <Aluno18@DESKTOP-5E468K0>
2019-12-18 22:33:58 -0200

    Aula 20 - 18/12/2019

---
+++ Aula20/usuarios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>App Cadastro de Usuários</title>
+</head>
+<body>
+
+	<a href="index.html">Voltar</a>
+	<h1>App Cadastro de Usuários</h1>
+        <a href="cadastrar.php"><button>Cadastrar usuário</button></a>
+        <br>
+        <a href="listar.php"><button>Listar Usuários Cadastrados</button></a>
+        <br>
+        <a href="procurar.php"><button>Procurar Usuário</button></a>
+        <br>
+        <a href="remover.php"><button>Remover Usuário</button></a>
+</body>
+</html>
